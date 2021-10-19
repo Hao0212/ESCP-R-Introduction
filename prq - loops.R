2f1bf9f619227b926579d41ad7be996164d9bd29
zhouhao <240985032@qq.com>
2021-10-19 11:36:09 +0200

    prq-loops

---
+++ prq - loops.R
@@ -0,0 +1,36 @@
+var1 = 25
+var2 = 35
+
+if((var1 + var2) > 50){
+  print("Ok")
+}else if(var1 + var2 > 75){
+  print("No")
+}else{
+  print("OOOOOK")
+}
+
+switch ("%",
+  
+  '1' = print("Monday").
+  '2' = print("Tuesday"),
+  '3' = print("Wednesday"),
+   print("Invalid Value")
+)
+
+repeat{
+  print(var1)
+  var1 = var1 + 2
+  if(var1 > 40){
+    break
+  }
+}
+
+while(var1 < 40){
+  print(var1)
+  var1 = var1 + 2
+}
+
+for(x in 1:25){
+  print(x)
+}
+
