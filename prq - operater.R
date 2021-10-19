abd9d01a68a3deaa275afe0f4fd23469beaa53c7
zhouhao <240985032@qq.com>
2021-10-19 11:27:44 +0200

    pre - operator

---
+++ prq - operater.R
@@ -0,0 +1,16 @@
+print(22/7)
+print(22%%7)
+print(2^7)
+
+print(22%/%7) #floor division
+
+var1 =25
+var2 = 60
+print(var1 == var2)
+
+
+value1 = c(TRUE)
+value2 = c(FALSE)
+print(value1 & value2)
+print(value1 | value2)
+print(!value1)
