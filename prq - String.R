3dd035e1071e7491bca21aaceec507258ba16e5b
zhouhao <240985032@qq.com>
2021-10-19 11:40:25 +0200

    prq-String

---
+++ prq - String.R
@@ -0,0 +1,15 @@
+str1 <- "Hey R is fun"
+print(str1)
+str2 <- "hello"
+
+str3 <- paste(str1,str2)
+print(str3)
+nchar(str3)
+
+str4 = toupper(str1)
+str5 = tolower(str2)
+print(str4)
+print(str5)
+
+str6 = substr(str3,4,7)
+print(str6)
